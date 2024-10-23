8f8a0032c959dbf83efe307aa9df34b6b0578a2e
Pxnch <fmontesq1600@alumno.ipn.mx>
2024-10-22 19:05:04 -0600

    Añadimos tailwind en Angular

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.ts',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
